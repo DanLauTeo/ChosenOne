56b1d1e060d1ce932d200eae08248d422a09a04f
TeoReu <reu.teodora@gmail.com>
2020-08-21 16:39:11 +0300

    Created some components

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProfileComponent} from './profile/profile.component'
+import {LoginComponent} from './login/login.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/app/src/app/app.module.ts
+++ frontend/app/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    TopBarComponent,
   ],
   imports: [
     BrowserModule,
