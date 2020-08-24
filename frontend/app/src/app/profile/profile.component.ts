d5ba2e7bdc05956e2e597a17fec4908f6e170c9e
TeoReu <reu.teodora@gmail.com>
2020-08-24 11:41:43 +0300

    Added mock_user, and created service account, and user_model

--- frontend/app/src/app/profile/profile.component.ts
+++ frontend/app/src/app/profile/profile.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service'
+import { Observable } from 'rxjs';
+import { USER } from '../_mock_models/mock_user'
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  user = USER;
+  constructor( private accountService : AccountService) {
+  }
 
   ngOnInit(): void {
   }
