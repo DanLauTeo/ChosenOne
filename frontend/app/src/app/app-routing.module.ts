29c3ded9a96cc212aa2d0a4507641a187ad58a63
TeoReu <reu.teodora@gmail.com>
2020-08-26 11:32:30 +0300

    Added auth.guard --> user can go to profile only if it's logged in (presses Join).

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ProfileComponent} from './profile/profile.component'
-import {LoginComponent} from './login/login.component'
+import {ProfileComponent} from './profile/profile.component';
+import {LoginComponent} from './login/login.component';
+import { AuthGuard } from './_helpers/auth.guard';
 
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent },
+  { path: '', component: ProfileComponent, canActivate: [AuthGuard]},
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent },
   
 ];
--- frontend/app/src/app/top-bar/top-bar.component.ts
+++ frontend/app/src/app/top-bar/top-bar.component.ts
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
