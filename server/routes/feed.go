2119ea1930b6aef51b11252a75ad3bfc58fcf596
TeoReu <reu.teodora@gmail.com>
2020-09-11 14:28:15 +0300

    Get images URL for feed

---
+++ server/routes/feed.go
@@ -0,0 +1,36 @@
+package routes
+
+import (
+	"localdev/main/models"
+	"localdev/main/services"
+	"net/http"
+	"encoding/json"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"cloud.google.com/go/datastore"
+)
+
+
+func GetPhotosForFeed(w http.ResponseWriter, r *http.Request) {
+	var response []string
+	ctx := appengine.NewContext(r)
+	query := datastore.NewQuery("Image")
+	it := services.Locator.DsClient().Run(ctx, query)
+	for {
+		var image models.Image
+		_, err := it.Next(&image)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Printf("Error fetching next image: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		response = append(([]string{image.GCSObjectID()}), response)
+	}
+	out, err := json.Marshal(response)
+	w.Write([]byte(out))
+}
+
