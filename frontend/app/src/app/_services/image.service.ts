1eee50ed0a1ecfba20f82606385a331a3f2547c3
Laura O'Donnell <laura.od@live.com>
2020-09-11 19:00:47 +0100

    Add image service for feed

---
+++ frontend/app/src/app/_services/image.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from  '@angular/common/http';  
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+  routeURL: string;
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getFeed() {
+    this.routeURL = "images";
+    return this.httpClient.get<any>(this.routeURL);
+  }
+}
