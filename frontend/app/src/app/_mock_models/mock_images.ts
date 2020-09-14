27313ba48db46db9267627c39ee6a0743c97032d
TeoReu <reu.teodora@gmail.com>
2020-09-14 14:32:04 +0300

    Mock users

---
+++ frontend/app/src/app/_mock_models/mock_images.ts
@@ -0,0 +1,34 @@
+import { Image } from '../_models/image'
+
+export const IMAGES: Image[] =[ 
+    {
+        imgURL: '../../assets/_mock_feed_gallery/1.jpg',
+        ownerID: '6',
+        ownerName: 'Irina Graur',
+        ownerProfile: '',
+    },
+    {
+        imgURL: '../../assets/_mock_feed_gallery/2.jpg',
+        ownerID: '7',
+        ownerName: 'Jonny Greeenwood',
+        ownerProfile: '',
+    },
+    {
+        imgURL: '../../assets/_mock_feed_gallery/3.jpg',
+        ownerID: '8',
+        ownerName: 'Boher Waters',
+        ownerProfile: '',
+    },
+    {
+        imgURL: '../../assets/_mock_feed_gallery/4.jpg',
+        ownerID: '10',
+        ownerName: 'Raluca Hoirup',
+        ownerProfile: '',
+    },
+    {
+        imgURL: '../../assets/_mock_feed_gallery/5.jpg',
+        ownerID: '10',
+        ownerName: 'Jolene Smith',
+        ownerProfile: '',
+    },
+]
